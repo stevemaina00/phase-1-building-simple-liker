01d6000a99cfb01589668312d723e563dd211587
Steve <steve.maina@student.moringaschool.com>
2022-08-29 12:15:08 +0300

    Complete assignment

--- main.js
+++ main.js
@@ -3,6 +3,29 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+let myModal = (document.querySelector("#modal").className = "hidden");
+let likeButtons = document.querySelectorAll(".like-glyph");
+likeButtons.forEach(function (likeButton) {
+	likeButton.addEventListener("click", function (e) {
+		mimicServerCall()
+			.then(function () {
+				if (likeButton.innerText === EMPTY_HEART) {
+					likeButton.innerText = FULL_HEART;
+					likeButton.className = "activated-heart";
+				} else {
+					likeButton.innerText = EMPTY_HEART;
+					likeButton.className = "";
+				}
+			})
+			.catch(function (error) {
+				myModal.className = "";
+				setTimeout(function () {
+					myModal.className = "hidden";
+				}, 3000);
+			});
+	});
+});
+
 
 
 
@@ -23,3 +46,4 @@ function mimicServerCall(url="http://mimicServer.example.com", config={}) {
     }, 300);
   });
 }
+
